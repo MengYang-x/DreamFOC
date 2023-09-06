5e505d23b0027de8fcd3872124da60100d49c7cd
稚子 <1973688584@qq.com>
2023-09-06 20:45:18 +0800

    数据备份

--- 5.softWare/3.SVPWM_C语言实现/FOC.cpp
+++ 5.softWare/3.SVPWM_C语言实现/FOC.cpp
@@ -2,12 +2,35 @@
 #include <math.h>
 #include "FOC.h"
 
+class FOC
+{
+private:
+public:
+    // float u_d;
+    // float u_q;
+
+    float u_alpha;
+    float u_beta;
+
+    void Inv_Park(float u_q, float u_d, float theta); // Park逆变换
+    void SVPWM();                                     // 计算矢量作用时间
+
+    float Udc = 12.5; // 输入电压
+    float Ts = 1.0;   // SVPWM的采样周期，设置为1.0，这样输出的Ta、Tb、Tc即为占空比
+
+    float Ta; // PWM占空比
+    float Tb;
+    float Tc;
+
+    void Open_velocity(); // 开环速度测试
+};
+
 /**********************************************************************************************************
 park逆变换：输入Uq、Ud得到Ualpha、Ubeta
 Uα = Ud · cosθ - Uq · sinθ
 Uβ = Ud · sinθ + Uq · cosθ
 **********************************************************************************************************/
-void FOC::Inv_Park()
+void FOC::Inv_Park(float u_q, float u_d, float theta)
 {
     float sinAngle = sin(theta);
     float cosAngle = cos(theta);
@@ -27,19 +50,22 @@ Step2：计算矢量作用时间Ta、Tb、Tc
 **********************************************************************************************************/
 void FOC::SVPWM()
 {
+    static float k = 1.73205 * Ts / Udc; // sqrt(3) ≈ 1.73205, K = sqrt(3) * Ts / Udc
+
     // 1.计算SVPWM算法中三个控制电压
-    float u1 = u_beta;                            // A
-    float u2 = 0.86603 * u_alpha - 0.5 * u_beta;  // B
-    float u3 = -0.86603 * u_alpha - 0.5 * u_beta; // C
+    float u1 = u_beta * k;                              // A
+    float u2 = (0.86603 * u_alpha - 0.5 * u_beta) * k;  // B
+    float u3 = (-0.86603 * u_alpha - 0.5 * u_beta) * k; // C
+
     // 2.扇区判断
     uint8_t sector = (u1 > 0.0) + ((u2 > 0.0) << 1) + ((u3 > 0.0) << 2); // sector = A + 2B + 4C
+
     // 3.计算三相PWM的占空比
-    static float k = 1.73205 * Ts / Udc; // sqrt(3) ≈ 1.73205, K = sqrt(3) * Ts / Udc
     switch (sector)
     {
     case 3: // 扇区1
-        float t4 = k * u2;
-        float t6 = k * u1;
+        float t4 = u2;
+        float t6 = u1;
         float sum = t4 + t6;
         if (sum > Ts)
         {
@@ -53,8 +79,8 @@ void FOC::SVPWM()
         Tc = t7;
         break;
     case 1: // 扇区2
-        float t2 = -k * u2;
-        float t6 = -k * u3;
+        float t2 = -u2;
+        float t6 = -u3;
         float sum = t2 + t6;
         if (sum > Ts)
         {
@@ -68,8 +94,8 @@ void FOC::SVPWM()
         Tc = t7;
         break;
     case 5: // 扇区3
-        float t2 = k * u1;
-        float t3 = k * u3;
+        float t2 = u1;
+        float t3 = u3;
         float sum = t2 + t3;
         if (sum > Ts)
         {
@@ -83,8 +109,8 @@ void FOC::SVPWM()
         Tc = t3 + t7;
         break;
     case 4: // 扇区4
-        float t1 = -k * u1;
-        float t3 = -k * u2;
+        float t1 = -u1;
+        float t3 = -u2;
         float sum = t1 + t3;
         if (sum > Ts)
         {
@@ -98,8 +124,8 @@ void FOC::SVPWM()
         Tc = t1 + t3 + t7;
         break;
     case 6: // 扇区5
-        float t1 = k * u3;
-        float t5 = k * u2;
+        float t1 = u3;
+        float t5 = u2;
         float sum = t1 + t5;
         if (sum > Ts)
         {
@@ -113,8 +139,8 @@ void FOC::SVPWM()
         Tc = t1 + t5 + t7;
         break;
     case 2: // 扇区6
-        float t4 = -k * u3;
-        float t5 = -k * u1;
+        float t4 = -u3;
+        float t5 = -u1;
         float sum = t4 + t5;
         if (sum > Ts)
         {
@@ -127,5 +153,20 @@ void FOC::SVPWM()
         Tb = t7;
         Tc = t5 + t7;
         break;
+    default:
+        break;
+    }
+}
+
+void FOC::Open_velocity()
+{
+    float u_d = 0;
+    float u_q = 2;
+
+    for (float theta = 0; theta < 6.2831853f; theta += 0.275f)
+    {
+        Inv_Park(u_q, u_d, theta);
+        SVPWM();
+        // 把占空比Ta、Tb、Tc输出进行测试
     }
 }
